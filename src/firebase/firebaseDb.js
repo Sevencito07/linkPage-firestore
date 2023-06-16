5f9c88f6b3f2f71afdac5509342d31908ce576d1
Mister G <108514442+Sevencito07@users.noreply.github.com>
2023-06-16 10:10:56 -0500

    Update firebaseDb.js

--- src/firebase/firebaseDb.js
+++ src/firebase/firebaseDb.js
@@ -4,12 +4,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD_CKKt0rE65eSbFd_P2svxIv10cWQ6n88",
-  authDomain: "list-page.firebaseapp.com",
-  projectId: "list-page",
-  storageBucket: "list-page.appspot.com",
-  messagingSenderId: "902522149327",
-  appId: "1:902522149327:web:2bfbd2c31a950d36311aba"
+ 
 };
 
 // Initialize Firebase
